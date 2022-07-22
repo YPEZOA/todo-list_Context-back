1b8203185c047c596f9a79ba85d60a07e7d0060c
YPEZOA <ypezoa.2brains@bancochile.cl>
2022-07-22 03:41:48 -0400

    feat: se modifica mensaje de respuesta

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,7 +5,7 @@ const login = async (req, res, next) => {
   const { user, password } = req.body;
   const findUser = await User.findOne({user});
   if(!findUser) return res.status(400).send({status: 400, message: 'Usuario no encontrado'})
-  if(findUser.password !== password) return res.status(400).send({status: 400, message: 'Contraseña incorrecta.'})
+  if(findUser.password !== password) return res.status(400).send({status: 400, message: 'Contraseña incorrecta'})
   console.log(findUser);
 
   const userData = await User.findOne({user}).select('_id user email');
@@ -35,21 +35,21 @@ const getUser = async (req, res) => {
 }
 
 const createUser = async (req, res, next) => {
-  const { email, userName, password } = req.body
+  const { email, user, password } = req.body
   const newUser = new User({
     email,
-    user: userName,
+    user,
     password,
     tasks: []
   })
 
-  if(!email || !userName || !password) {
-    return res.status(400).send({status: 400, message: "Valida los datos solicitados", response: req.body})
+  if(!email || !user || !password) {
+    return res.status(400).send({status: 400, message: "Validar datos", response: req.body})
   }
 
   // Validate if email exist
-  const user = await User.findOne({ email })
-  if (user) {
+  const currentUser = await User.findOne({ email })
+  if (currentUser) {
     return res
       .status(400)
       .send({ status: 400, message: 'Email de usuario ya existe' })
@@ -57,12 +57,10 @@ const createUser = async (req, res, next) => {
 
   try {
     await newUser.save()
-    const token = jwt.sign({ _id: newUser._id }, 'secretkey')
-
     return res.status(200).send({
+      status: 200,
       message: 'Registro de usuario exitoso',
       data: newUser,
-      token
     })
   } catch (error) {
     next(error)
